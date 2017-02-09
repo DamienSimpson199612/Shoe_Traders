acd6e15cc064f3d1930dd547a2236baf9df9cd7c
P15227038 <P15227038@GWHXX682WIN019W>
2017-02-09 16:54:16 +0000

    Creation of clsStaff + test class

---
+++ Test Framework/tstStaff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_Library;
+
+namespace Test_Framework
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of out class staff
+            clsStaff Staff = new clsStaff();
+            //check to see that the class is not null
+            Assert.IsNotNull(Staff);
+
+        }
+
+    }
+}
