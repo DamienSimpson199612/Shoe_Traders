9d647e7e10aa63efb930cb26209fe74923267fa3
P15227038 <P15227038@GWHXX682WIN019W>
2017-02-16 16:34:51 +0000

    Tests for clsStaff have been started and will cary on being worked on

--- Class Library/clsStaff.cs
+++ Class Library/clsStaff.cs
@@ -1,6 +1,42 @@
-﻿namespace Class_Library
+﻿using System;
+
+namespace Class_Library
 {
     public class clsStaff
     {
+        //private data member for the StaffID property
+        private Int32 mStaffID;
+
+        public bool Active { get; set; }
+        public string Address { get; set; }
+        public string County { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int MobileNo { get; set; }
+        public int PayrollNo { get; set; }
+        public string PostCode { get; set; }
+        public string RoleType { get; set; }
+        //public property for the Staff number
+        public int StaffID
+        {
+            get
+            {//return the private data 
+                return mStaffID;
+            }
+            set
+            {
+                //set the value if the priavte data memeber
+                mStaffID = value;
+            }
+        }
+        public string TrainingCompleted { get; set; }
+
+        public bool Find(int staffID)
+        {
+            //set the private data 
+            mStaffID = 21;
+            //always return true
+            return true;
+        }
     }
 }
--- Test Framework/tstStaff.cs
+++ Test Framework/tstStaff.cs
@@ -5,17 +5,213 @@ using Class_Library;
 namespace Test_Framework
 {
     [TestClass]
-    public class tstStaff
+    public class TestLog_clsStaff
     {
         [TestMethod]
         public void InstanceOK()
         {
-            //create an instance of out class staff
+            //create an instance of the class we want to create
             clsStaff Staff = new clsStaff();
-            //check to see that the class is not null
+            //test to see that it exists
             Assert.IsNotNull(Staff);
+        }
+
+
+        [TestMethod]
+        public void ActiveStaffMember()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            Boolean TestData = true;
+            //asign the data to the property
+            Staff.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.Active, TestData);
 
         }
 
+        [TestMethod]
+        public void Address()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            String TestData = "21b";
+            //asign the data to the property
+            Staff.Address = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.Address, TestData);
+
+        }
+
+        [TestMethod]
+        public void County()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            String TestData = "Nottinghamshire";
+            //asign the data to the property
+            Staff.County = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.County, TestData);
+
+        }
+
+        [TestMethod]
+        public void FirstName()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            String TestData = "Dez";
+            //asign the data to the property
+            Staff.FirstName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.FirstName, TestData);
+
+        }
+
+        [TestMethod]
+        public void LastName()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            String TestData = "Oppang";
+            //asign the data to the property
+            Staff.LastName = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.LastName, TestData);
+
+        }
+
+        [TestMethod]
+        public void MobileNo()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            Int32 TestData = 1;
+            //asign the data to the property
+            Staff.MobileNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.MobileNo, TestData);
+
+        }
+
+        [TestMethod]
+        public void PayrollNo()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            Int32 TestData = 1;
+            //asign the data to the property
+            Staff.PayrollNo = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.PayrollNo, TestData);
+
+        }
+
+        [TestMethod]
+        public void PostCode()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            String TestData = "LE27EF";
+            //asign the data to the property
+            Staff.PostCode = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.PostCode, TestData);
+
+        }
+
+        [TestMethod]
+        public void RoleType()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            String TestData = "Manager";
+            //asign the data to the property
+            Staff.RoleType = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.RoleType, TestData);
+
+        }
+
+        [TestMethod]
+        public void StaffID()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            Int32 TestData = 1;
+            //asign the data to the property
+            Staff.StaffID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.StaffID, TestData);
+
+        }
+
+        [TestMethod]
+        public void TrainingCompleted()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //create some test data tp assign to the property
+            String TestData = "FirstAid";
+            //asign the data to the property
+            Staff.TrainingCompleted = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(Staff.TrainingCompleted, TestData);
+
+        }
+
+        [TestMethod]
+        public void FindMethodOK()
+        {
+
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //boolean variable to store the result of the validation
+            Boolean Found = false;
+            //create some test data to use with the method
+            Int32 StaffID = 1;
+            //invoke the method
+            Found = Staff.Find(StaffID);
+            //test to see that the result is correct
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestAddressFound()
+        {
+            //create an instance of the class we want to create
+            clsStaff Staff = new clsStaff();
+            //boolean variable to store the result of the search
+            Boolean Found = false;
+            //boolean variable to record if data is OK (assume it is)
+            Boolean OK = true;
+            //create some test data tp assign to the property
+            Int32 StaffID = 21;
+            //invoke the method
+            Found = Staff.Find(StaffID);
+            //check the address no
+            if (Staff.StaffID != 21)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+
     }
+
 }
+
