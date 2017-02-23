2688afcec358beed258be5458d7bba1e7fb3efbc
P15227038 <P15227038@GWHXX682WIN019W>
2017-02-23 12:00:22 +0000

    Continued witht the find method

--- Class Library/clsStaff.cs
+++ Class Library/clsStaff.cs
@@ -6,11 +6,37 @@ namespace Class_Library
     {
         //private data member for the StaffID property
         private Int32 mStaffID;
+        //private data member for the StaffID property
+        private String mCounty;
+        //private data member for the StaffID property
+        private String mFirstName;
 
         public bool Active { get; set; }
         public string Address { get; set; }
-        public string County { get; set; }
-        public string FirstName { get; set; }
+        public string County
+        {
+            get
+            { //return the private data 
+                return mCounty;
+            }
+            set
+            {
+                //set the value if the priavte data memeber
+                mCounty = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            { //return the private data 
+                return mFirstName;
+            }
+            set
+            {
+                //set the value if the priavte data memeber
+                mFirstName= value;
+            }
+        }
         public string LastName { get; set; }
         public int MobileNo { get; set; }
         public int PayrollNo { get; set; }
@@ -35,8 +61,14 @@ namespace Class_Library
         {
             //set the private data 
             mStaffID = 21;
+            //set the private data 
+            mCounty = "Test County";
+            //set the private data 
+            mFirstName = "Test First Name";
             //always return true
             return true;
         }
+
+      
     }
 }
