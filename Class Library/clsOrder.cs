50a40fcfbd1cfac8aa340ae9b8eeb95fa8a8ee0b
p1523878x <p1523878x@GWHXX581WIN019W>
2017-02-23 11:19:13 +0000

    added tst order

---
+++ Class Library/clsOrder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class clsOrder
+    {
+        //private data member for order No
+        private Int32 mOrderNo;
+        public clsOrder()
+        {
+
+        }
+        public int OrderNo
+        {
+            get
+            {
+                //return private data
+                return mOrderNo;
+            }
+            set
+            {
+                //set the value of the private data member
+                mOrderNo = value;
+            }
+        }
+
+        public bool Active { get; set; }
+        public string CustomerName { get; set; }
+        public int CustomerNo { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int NumberOfOrder { get; set; }
+
+
+        public int Price { get; set; }
+
+        public bool Find(int OrderNo)
+        {
+            //set private data member to test the data value
+            mOrderNo = 21;
+            //always return value
+            return true;
+        }
+    }
+}
