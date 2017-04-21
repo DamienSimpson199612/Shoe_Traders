0370e275ca1f374d8ec5e6e36e5d1d3e6390faa9
P15227038 <P15227038@GWHXX682WIN019W>
2017-04-21 13:02:08 +0100

    Added Qays Pages

---
+++ FrontEnd/AnCustomer.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AnCustomer.aspx.cs" Inherits="AnCustomer" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+    <asp:TextBox ID="txtOrderNo" runat="server"></asp:TextBox>
+&nbsp;<asp:Label ID="lblOrderNo" runat="server" Text="OrderNo"></asp:Label>
+    <br />
+    <br />
+    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+    <asp:Label ID="lblName" runat="server" Text="Customer Name"></asp:Label>
+    <br />
+    <br />
+    <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+    <asp:Label ID="lblAddress" runat="server" Text="Address"></asp:Label>
+    <br />
+    <br />
+    <asp:TextBox ID="txtPostCode" runat="server"></asp:TextBox>
+    <asp:Label ID="lblPostCode" runat="server" Text="Postcode"></asp:Label>
+    <br />
+    <br />
+    <asp:TextBox ID="txtContactNumber" runat="server"></asp:TextBox>
+    <asp:Label ID="lblContactNumber" runat="server" Text="ContactNumber"></asp:Label>
+    <br />
+    <br />
+    <asp:TextBox ID="txtEmailAddress" runat="server"></asp:TextBox>
+    <asp:Label ID="lblEmailAddress" runat="server" Text="Email Address"></asp:Label>
+    <br />
+    <br />
+    <asp:TextBox ID="txtDateJoined" runat="server"></asp:TextBox>
+    <asp:Label ID="lblDateJoined" runat="server" Text="Date Joined"></asp:Label>
+    <br />
+    <br />
+    <asp:CheckBox ID="Active" runat="server" />
+    <br />
+    <br />
+    <asp:Label ID="lblError" runat="server" Text="Label"></asp:Label>
+    <br />
+    <br />
+    <asp:Button ID="btnOK" runat="server" Text="OK" />
+    <asp:Button ID="btnCancel" runat="server" Text="Cancel" />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+</asp:Content>
+
--- FrontEnd/AnOrder.aspx.cs
+++ FrontEnd/AnOrder.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Class_Library;
+using MyClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,17 +8,83 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class AnOrder : System.Web.UI.Page
+
+
 {
+    //var to store the address number
+    Int32 OrderNo;
     protected void Page_Load(object sender, EventArgs e)
     {
+        //get the address no from the session object
+        OrderNo = Convert.ToInt32(Session["OrderNo"]);
+        //if this is the first time the page has loaded
+        if (IsPostBack == false)
+        {
+            //Populate the counties drop down
+            DisplayCustomers();
+            //if we are not adding a new record
+            if (OrderNo != -1)
+            {
 
+                //update the fields on the page with the data from the record
+                DisplayOrders();
+            }
+            else//this is the new record
+            {
+                //set the date to todays date
+                txtOrderDate.Text = DateTime.Today.Date.ToString("dd/MM/yyy");
+            }
+        }
     }
 
     protected void btnOk_Click(object sender, EventArgs e)
     {
-
+        //var to store error messages
+        string ErrorMsg;
+        // //create an instance of the address class
+        clsOrderCollection OrderFile = new clsOrderCollection();
+        //use the objects validation method to test the data
+        ErrorMsg = OrderFile.ThisOrder.Valid(txtCustomerName.Text, txtNumberOfOrder.Text, txtOrderDate.Text);
+        //if there is no error message
+        if (ErrorMsg == "")
+        {
+            //if we are adding a new record
+            if (OrderNo == -1)
+            {
+                //set the property
+                OrderFile.ThisOrder.CustomerName = txtCustomerName.Text;
+                //set the property
+                OrderFile.ThisOrder.NumberOfOrder = Convert.ToInt32(txtNumberOfOrder.Text);
+                //set the property
+                OrderFile.ThisOrder.OrderDate = Convert.ToDateTime(txtOrderDate);
+                //set the property
+                OrderFile.ThisOrder.Active = Active.Checked;
+                //invoke the add method
+                OrderFile.Add();
+            }
+            else
+            {
+                OrderFile.ThisOrder.Find(OrderNo);
+                //set the property
+                OrderFile.ThisOrder.CustomerName = txtCustomerName.Text;
+                //set the property
+                OrderFile.ThisOrder.NumberOfOrder = Convert.ToInt32(txtNumberOfOrder.Text);
+                //set the property
+                OrderFile.ThisOrder.OrderDate = Convert.ToDateTime(txtOrderDate);
+                //set the property
+                OrderFile.ThisOrder.Active = Active.Checked;
+                //update record
+                OrderFile.Update();
+            }
+            //all done so redirect back to the main page
+            Response.Redirect("Orders.aspx");
+        }
+        else//there are errors
+        {
+            //display the error message
+            lblError.Text = ErrorMsg;
+        }
     }
-
     protected void btnCancel_Click(object sender, EventArgs e)
     {
 
